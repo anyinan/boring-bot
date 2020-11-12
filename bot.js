90444a0c7a3f919d0d5de81b914d961b062e0e24
MAGKILLER <jacklishijia@gmail.com>
2020-11-11 16:14:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ client.on('message', msg => {
 			request('http://www.google.com', function (error, response, body) {
 				 console.error('error:', error); // Print the error if one occurred
 				 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-				 msg.reply('body' );
+				 msg.reply(body);
 			});   
 		}
 	}
