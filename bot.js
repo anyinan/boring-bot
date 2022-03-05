ff06c4730909c5da6b86ddc0ef1bcdf789709372
MAGKILLER <jacklishijia@gmail.com>
2022-03-05 02:00:04 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -234,5 +234,31 @@ var roleRef = {
 // });
 
 
+const { SpeechSynthesisOutputFormat, SpeechConfig, AudioConfig, SpeechSynthesizer } = require("microsoft-cognitiveservices-speech-sdk");
+
+function synthesizeSpeech() {    
+    //replace "YourSubscriptionKey" and "YourServiceRegion" with your azure key and region
+    //e.g. const speechConfig = SpeechConfig.fromSubscription("29f3f317abe376fd3b2a9b773112646d", "westeurope");
+    const speechConfig = SpeechConfig.fromSubscription(process.env.AZURE_TOKEN, "westus");
+    speechConfig.speechSynthesisOutputFormat = SpeechSynthesisOutputFormat.Audio24Khz160KBitRateMonoMp3;
+    const audioConfig = AudioConfig.fromAudioFileOutput("output.mp3");
+
+    const synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+    synthesizer.speakTextAsync(
+        "A simple test to write to a file.",
+        result => {
+            if (result) {
+                console.log(JSON.stringify(result));
+            }
+            synthesizer.close();
+        },
+        error => {
+            console.log(error);
+            synthesizer.close();
+        });
+}
+
+synthesizeSpeech();
+
 client.login(process.env.BOT_TOKEN);
 //client.login(token);
